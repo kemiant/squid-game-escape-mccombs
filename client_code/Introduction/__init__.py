1c32e0ce623c54b7fb2e7a0b283080b5c23bb9d8
cutemusic6894@gmail.com <cutemusic6894@gmail.com>
2024-10-20 23:51:49 +0000

    Created ServerModule1 and Introduction; Edited settings and Form1

--- client_code/Introduction/__init__.py
+++ client_code/Introduction/__init__.py
@@ -1,9 +1,9 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import IntroductionTemplate
 from anvil import *
 import anvil.server
 
 
-class Form1(Form1Template):
+class Introduction(IntroductionTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
