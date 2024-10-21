eaad2fa2a92a1392c4f7dcaca7455e1a82ff28bc
cutemusic6894@gmail.com <cutemusic6894@gmail.com>
2024-10-21 14:56:35 +0000

    Created P1_Red_Light, P2_Glass_Steps, P3_Sugar_Cookies, P4_Tug_War and 3 more; Edited theme.css, settings and theme/parameters.yaml; Deleted custom package dependencies

---
+++ client_code/P6_Old_Man/__init__.py
@@ -0,0 +1,44 @@
+from ._anvil_designer import P6_Old_ManTemplate
+from anvil import *
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+from anvil import Timer
+import anvil.js
+
+
+class P6_Old_Man(P6_Old_ManTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.text_to_display1 = """"""
+    self.text_to_display2 = """"""
+    # Counter for the position of next character to display
+    self.current_position = 0
+    # Start the typing effect
+    anvil.js.call_js("startTypingEffect", self.type_text)
+
+  def type_text(self):
+    # Remove cursor if it's there
+    if self.label_1.text.endswith("▮"):
+      self.label_1.text = self.label_1.text[:-1]
+
+    if self.current_position < len(self.text_to_display1):
+      # Append next character to label's text
+      self.label_1.text += self.text_to_display1[self.current_position]
+      self.current_position += 1
+      # Add cursor
+      self.label_1.text += "▮"
+    else:
+      # Stop the typing effect
+      anvil.js.call_js("stopTypingEffect")
+      self.label_1.visible = False
+      self.rich_text_1.visible = True
+      self.rich_text_1.content = self.text_to_display2
+
+  def submit_click(self, **event_args):
+    combination_value = self.combination_lock_form.get_combination()
+    if anvil.server.call_s("", combination_value):
+      open_form("")
---
+++ client_code/combination_lock/__init__.py
@@ -0,0 +1,64 @@
+from ._anvil_designer import combination_lock1Template
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class combination_lock1(combination_lock1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+
+  def digit1_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
+    if len(self.digit1.text) > 1:
+      self.digit1.text = self.digit1.text[1]
+    if len(self.digit1.text) == 1:
+      self.digit2.focus()
+
+  def digit2_change(self, **event_args):
+    if len(self.digit2.text) > 1:
+      self.digit2.text = self.digit2.text[1]
+    if len(self.digit2.text) == 1:
+      self.digit3.focus()
+
+  def digit3_change(self, **event_args):
+    if len(self.digit3.text) > 1:
+      self.digit3.text = self.digit3.text[1]
+    if len(self.digit3.text) == 1:
+      self.digit4.focus()
+
+  def digit4_change(self, **event_args):
+    if len(self.digit4.text) > 1:
+      self.digit4.text = self.digit4.text[1]
+    if len(self.digit4.text) == 1:
+      self.digit5.focus()
+
+  def digit5_change(self, **event_args):
+    if len(self.digit5.text) > 1:
+      self.digit5.text = self.digit5.text[1]
+    if len(self.digit5.text) == 1:
+      self.digit6.focus()
+
+  def digit6_change(self, **event_args):
+    if len(self.digit6.text) > 1:
+      self.digit6.text = self.digit6.text[1]
+
+  def get_combination(self):
+    return ''.join([self.digit1.text, self.digit2.text, self.digit3.text, self.digit4.text, self.digit5.text, self.digit6.text])
+
+  def clear_values(self):
+        # Assuming digit1, digit2, ..., digit6 are the TextBox component names
+        self.digit1.text = ''
+        self.digit2.text = ''
+        self.digit3.text = ''
+        self.digit4.text = ''
+        self.digit5.text = ''
+        self.digit6.text = ''
+
+  def flash_effect(self):
+    anvil.js.call_js('flashUnderlineColorByClass', 'anvil-role-combination-lock', 3000)
+
