b1d3d4e01f47a6d9bdf9e148d597ff6cc4e48228
cutemusic6894@gmail.com <cutemusic6894@gmail.com>
2024-10-21 00:16:18 +0000

    Created C1_RedLight; Deleted Form1

--- client_code/C1_RedLight/__init__.py
+++ client_code/C1_RedLight/__init__.py
@@ -1,9 +1,9 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import C1_RedLightTemplate
 from anvil import *
 import anvil.server
 
 
-class Form1(Form1Template):
+class C1_RedLight(C1_RedLightTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
